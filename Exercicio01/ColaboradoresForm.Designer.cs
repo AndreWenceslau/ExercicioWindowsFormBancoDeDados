c90918943546be835c678a357eea74d3ea6d6a66
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-06-19 11:22:05 -0300

    Finalizando botão salvar colaboradores

--- Exercicio01/ColaboradoresForm.Designer.cs
+++ Exercicio01/ColaboradoresForm.Designer.cs
@@ -40,7 +40,7 @@
             this.cbCargo = new System.Windows.Forms.ComboBox();
             this.lblProgramador = new System.Windows.Forms.Label();
             this.cbSexo = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvColaboradores = new System.Windows.Forms.DataGridView();
             this.ColumnCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnCPF = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -52,17 +52,16 @@
             this.btnSalvar = new System.Windows.Forms.Button();
             this.ckbProgramador = new System.Windows.Forms.CheckBox();
             this.mtbSalario = new System.Windows.Forms.MaskedTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColaboradores)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCodigo
             // 
             this.lblCodigo.AutoSize = true;
             this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCodigo.Location = new System.Drawing.Point(13, 92);
-            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Location = new System.Drawing.Point(10, 75);
             this.lblCodigo.Name = "lblCodigo";
-            this.lblCodigo.Size = new System.Drawing.Size(92, 29);
+            this.lblCodigo.Size = new System.Drawing.Size(71, 24);
             this.lblCodigo.TabIndex = 0;
             this.lblCodigo.Text = "Código";
             this.lblCodigo.Click += new System.EventHandler(this.label1_Click);
@@ -71,10 +70,9 @@
             // 
             this.lblId.AutoSize = true;
             this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblId.Location = new System.Drawing.Point(113, 92);
-            this.lblId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblId.Location = new System.Drawing.Point(85, 75);
             this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(26, 29);
+            this.lblId.Size = new System.Drawing.Size(20, 24);
             this.lblId.TabIndex = 1;
             this.lblId.Text = "0";
             // 
@@ -82,10 +80,9 @@
             // 
             this.lblNome.AutoSize = true;
             this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNome.Location = new System.Drawing.Point(13, 133);
-            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNome.Location = new System.Drawing.Point(10, 108);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(79, 29);
+            this.lblNome.Size = new System.Drawing.Size(62, 24);
             this.lblNome.TabIndex = 2;
             this.lblNome.Text = "Nome";
             this.lblNome.Click += new System.EventHandler(this.label1_Click_1);
@@ -93,10 +90,9 @@
             // txtNome
             // 
             this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNome.Location = new System.Drawing.Point(100, 133);
-            this.txtNome.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNome.Location = new System.Drawing.Point(75, 108);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(179, 34);
+            this.txtNome.Size = new System.Drawing.Size(135, 28);
             this.txtNome.TabIndex = 3;
             this.txtNome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -104,10 +100,9 @@
             // 
             this.lblCPF.AutoSize = true;
             this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCPF.Location = new System.Drawing.Point(13, 175);
-            this.lblCPF.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCPF.Location = new System.Drawing.Point(10, 142);
             this.lblCPF.Name = "lblCPF";
-            this.lblCPF.Size = new System.Drawing.Size(61, 29);
+            this.lblCPF.Size = new System.Drawing.Size(47, 24);
             this.lblCPF.TabIndex = 4;
             this.lblCPF.Text = "CPF";
             // 
@@ -115,22 +110,21 @@
             // 
             this.lblSalario.AutoSize = true;
             this.lblSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSalario.Location = new System.Drawing.Point(13, 220);
-            this.lblSalario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSalario.Location = new System.Drawing.Point(10, 179);
             this.lblSalario.Name = "lblSalario";
-            this.lblSalario.Size = new System.Drawing.Size(89, 29);
+            this.lblSalario.Size = new System.Drawing.Size(67, 24);
             this.lblSalario.TabIndex = 6;
             this.lblSalario.Text = "Salário";
             this.lblSalario.Click += new System.EventHandler(this.label1_Click_2);
             // 
             // mtbCPF
             // 
+            this.mtbCPF.Culture = new System.Globalization.CultureInfo("");
             this.mtbCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mtbCPF.Location = new System.Drawing.Point(100, 175);
-            this.mtbCPF.Margin = new System.Windows.Forms.Padding(4);
+            this.mtbCPF.Location = new System.Drawing.Point(75, 142);
             this.mtbCPF.Mask = "999.999.999-99";
             this.mtbCPF.Name = "mtbCPF";
-            this.mtbCPF.Size = new System.Drawing.Size(179, 34);
+            this.mtbCPF.Size = new System.Drawing.Size(135, 28);
             this.mtbCPF.TabIndex = 7;
             this.mtbCPF.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtbCPF_MaskInputRejected);
             // 
@@ -138,10 +132,9 @@
             // 
             this.lblSexo.AutoSize = true;
             this.lblSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSexo.Location = new System.Drawing.Point(13, 266);
-            this.lblSexo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSexo.Location = new System.Drawing.Point(10, 216);
             this.lblSexo.Name = "lblSexo";
-            this.lblSexo.Size = new System.Drawing.Size(68, 29);
+            this.lblSexo.Size = new System.Drawing.Size(54, 24);
             this.lblSexo.TabIndex = 9;
             this.lblSexo.Text = "Sexo";
             // 
@@ -149,10 +142,9 @@
             // 
             this.lblCargo.AutoSize = true;
             this.lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCargo.Location = new System.Drawing.Point(13, 310);
-            this.lblCargo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCargo.Location = new System.Drawing.Point(10, 252);
             this.lblCargo.Name = "lblCargo";
-            this.lblCargo.Size = new System.Drawing.Size(79, 29);
+            this.lblCargo.Size = new System.Drawing.Size(61, 24);
             this.lblCargo.TabIndex = 12;
             this.lblCargo.Text = "Cargo";
             this.lblCargo.Click += new System.EventHandler(this.label1_Click_3);
@@ -164,20 +156,18 @@
             this.cbCargo.Items.AddRange(new object[] {
             "Padeiro",
             "Motorista "});
-            this.cbCargo.Location = new System.Drawing.Point(100, 302);
-            this.cbCargo.Margin = new System.Windows.Forms.Padding(4);
+            this.cbCargo.Location = new System.Drawing.Point(75, 245);
             this.cbCargo.Name = "cbCargo";
-            this.cbCargo.Size = new System.Drawing.Size(179, 37);
+            this.cbCargo.Size = new System.Drawing.Size(135, 30);
             this.cbCargo.TabIndex = 13;
             // 
             // lblProgramador
             // 
             this.lblProgramador.AutoSize = true;
             this.lblProgramador.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProgramador.Location = new System.Drawing.Point(13, 364);
-            this.lblProgramador.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblProgramador.Location = new System.Drawing.Point(10, 296);
             this.lblProgramador.Name = "lblProgramador";
-            this.lblProgramador.Size = new System.Drawing.Size(155, 29);
+            this.lblProgramador.Size = new System.Drawing.Size(120, 24);
             this.lblProgramador.TabIndex = 14;
             this.lblProgramador.Text = "Programador";
             // 
@@ -188,17 +178,18 @@
             this.cbSexo.Items.AddRange(new object[] {
             "Masculino",
             "Feminino"});
-            this.cbSexo.Location = new System.Drawing.Point(100, 258);
+            this.cbSexo.Location = new System.Drawing.Point(75, 210);
+            this.cbSexo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbSexo.Name = "cbSexo";
-            this.cbSexo.Size = new System.Drawing.Size(179, 37);
+            this.cbSexo.Size = new System.Drawing.Size(135, 30);
             this.cbSexo.TabIndex = 17;
             // 
-            // dataGridView1
+            // dgvColaboradores
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvColaboradores.AllowUserToAddRows = false;
+            this.dgvColaboradores.AllowUserToDeleteRows = false;
+            this.dgvColaboradores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvColaboradores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColumnCodigo,
             this.ColumnNome,
             this.ColumnCPF,
@@ -206,12 +197,14 @@
             this.ColumnSexo,
             this.ColumnCargo,
             this.ColumnProgramador});
-            this.dataGridView1.Location = new System.Drawing.Point(363, 8);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(739, 534);
-            this.dataGridView1.TabIndex = 18;
+            this.dgvColaboradores.Location = new System.Drawing.Point(272, 6);
+            this.dgvColaboradores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvColaboradores.Name = "dgvColaboradores";
+            this.dgvColaboradores.ReadOnly = true;
+            this.dgvColaboradores.RowTemplate.Height = 24;
+            this.dgvColaboradores.Size = new System.Drawing.Size(542, 434);
+            this.dgvColaboradores.TabIndex = 18;
+            this.dgvColaboradores.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvColaboradores_CellDoubleClick);
             // 
             // ColumnCodigo
             // 
@@ -257,18 +250,20 @@
             // 
             // btnExcluir
             // 
-            this.btnExcluir.Location = new System.Drawing.Point(176, 415);
+            this.btnExcluir.Location = new System.Drawing.Point(132, 337);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(159, 72);
+            this.btnExcluir.Size = new System.Drawing.Size(119, 58);
             this.btnExcluir.TabIndex = 19;
             this.btnExcluir.Text = "Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(9, 415);
+            this.btnSalvar.Location = new System.Drawing.Point(7, 337);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(159, 72);
+            this.btnSalvar.Size = new System.Drawing.Size(119, 58);
             this.btnSalvar.TabIndex = 20;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
@@ -277,32 +272,34 @@
             // ckbProgramador
             // 
             this.ckbProgramador.AutoSize = true;
-            this.ckbProgramador.Location = new System.Drawing.Point(176, 371);
+            this.ckbProgramador.Location = new System.Drawing.Point(132, 301);
+            this.ckbProgramador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ckbProgramador.Name = "ckbProgramador";
-            this.ckbProgramador.Size = new System.Drawing.Size(113, 21);
+            this.ckbProgramador.Size = new System.Drawing.Size(43, 17);
             this.ckbProgramador.TabIndex = 21;
-            this.ckbProgramador.Text = "Programador";
+            this.ckbProgramador.Text = "Sim";
             this.ckbProgramador.UseVisualStyleBackColor = true;
             // 
             // mtbSalario
             // 
             this.mtbSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mtbSalario.Location = new System.Drawing.Point(100, 220);
+            this.mtbSalario.Location = new System.Drawing.Point(75, 179);
+            this.mtbSalario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.mtbSalario.Mask = "$9999.99";
             this.mtbSalario.Name = "mtbSalario";
-            this.mtbSalario.Size = new System.Drawing.Size(179, 34);
+            this.mtbSalario.Size = new System.Drawing.Size(135, 28);
             this.mtbSalario.TabIndex = 22;
             // 
             // ColaboradoresForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1116, 554);
+            this.ClientSize = new System.Drawing.Size(832, 450);
             this.Controls.Add(this.mtbSalario);
             this.Controls.Add(this.ckbProgramador);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.btnExcluir);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvColaboradores);
             this.Controls.Add(this.cbSexo);
             this.Controls.Add(this.lblProgramador);
             this.Controls.Add(this.cbCargo);
@@ -315,10 +312,10 @@
             this.Controls.Add(this.lblNome);
             this.Controls.Add(this.lblId);
             this.Controls.Add(this.lblCodigo);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ColaboradoresForm";
             this.Text = "ColaboradoresForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.ColaboradoresForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColaboradores)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -338,7 +335,7 @@
         private System.Windows.Forms.ComboBox cbCargo;
         private System.Windows.Forms.Label lblProgramador;
         private System.Windows.Forms.ComboBox cbSexo;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvColaboradores;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCodigo;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCPF;
--- Exercicio01/ColaboradoresForm.cs
+++ Exercicio01/ColaboradoresForm.cs
@@ -45,42 +45,44 @@ namespace Exercicio01
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            if(lblId.Text == "0")
+            if (lblId.Text == "0")
             {
-                //Inserir();
+                Inserir();
             }
             else
             {
-                //Alterar();
+                Alterar();
             }
         }
         private void Inserir()
         {
-            ColaboradoresClasse colaboradores = new ColaboradoresClasse();
+            Colaborador colaboradores = new Colaborador();
             colaboradores.Nome = txtNome.Text;
             colaboradores.Cpf = mtbCPF.Text;
-            colaboradores.Salario = Convert.ToDecimal(mtbSalario.Text);
+            colaboradores.Salario = Convert.ToDecimal(mtbSalario.Text.Replace("R$", ""));
             colaboradores.Sexo = cbSexo.SelectedItem.ToString();
             colaboradores.Cargo = cbCargo.SelectedItem.ToString();
+            colaboradores.Programador = ckbProgramador.Checked;
+
 
             SqlConnection conexao = new SqlConnection();
-            conexao.ConnectionString = @"";
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\Banco-de-dados.mdf;Integrated Security=True;Connect Timeout=30";
             conexao.Open();
 
             SqlCommand comando = new SqlCommand();
             comando.Connection = conexao;
             comando.CommandText = @"INSERT INTO colaboradores(nome, cpf, salario, sexo, cargo,programador)VALUES(@NOME,@CPF,@SALARIO,@SEXO,@CARGO,@PROGRAMADOR)";
-            comando.Parameters.AddWithValue("@NOME",colaboradores.Nome);
+            comando.Parameters.AddWithValue("@NOME", colaboradores.Nome);
             comando.Parameters.AddWithValue("@CPF", colaboradores.Cpf);
             comando.Parameters.AddWithValue("@SALARIO", colaboradores.Salario);
             comando.Parameters.AddWithValue("@SEXO", colaboradores.Sexo);
             comando.Parameters.AddWithValue("@CARGO", colaboradores.Cargo);
-
+            comando.Parameters.AddWithValue("@PROGRAMADOR", colaboradores.Programador);
             comando.ExecuteNonQuery();
             MessageBox.Show("Registro salvo com sucesso");
-            //LimparCampos();
+            LimparCampos();
             conexao.Close();
-            //AtualizarTabela();
+            AtualizarTabela();
         }
         private void LimparCampos()
         {
@@ -91,6 +93,73 @@ namespace Exercicio01
             cbSexo.SelectedIndex = -1;
             cbCargo.SelectedIndex = -1;
         }
+        private void AtualizarTabela()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\Banco-de-dados.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.Open();
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = "SELECT id,nome,cpf,salario,sexo,cargo,programador FROM colaboradores";
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+            dgvColaboradores.RowCount = 0;
+            for (int i = 0; i < tabela.Rows.Count; i++)
+            {
+                DataRow linha = tabela.Rows[i];
+                Colaborador colaborador = new Colaborador();
+                colaborador.Id = Convert.ToInt32(linha["id"]);
+                colaborador.Nome = linha["nome"].ToString();
+                colaborador.Cpf = linha["cpf"].ToString();
+                colaborador.Salario = Convert.ToDecimal(linha["salario"]);
+                colaborador.Sexo = linha["sexo"].ToString();
+                colaborador.Cargo = linha["cargo"].ToString();
+                colaborador.Programador = Convert.ToBoolean(linha["programador"]);
+                string texto = "";
+                if (colaborador.Programador == true)
+                {
+                    texto = "Sim";
+                }
+                else
+                {
+                    texto = "Não";
+                }
+
+
+                dgvColaboradores.Rows.Add(new string[] { colaborador.Id.ToString(), colaborador.Nome.ToString(), colaborador.Cpf.ToString(), colaborador.Salario.ToString(), colaborador.Sexo, colaborador.Cargo, texto });
+            }
+        }
+        private void Alterar()
+        {
+            Colaborador colaborador = new Colaborador();
+            colaborador.Id = Convert.ToInt32(lblId.Text);
+            colaborador.Nome = Convert.ToString(txtNome.Text);
+            colaborador.Cpf = Convert.ToString(mtbCPF.Text);
+            colaborador.Salario = Convert.ToDecimal(mtbSalario.Text.Replace("R$",""));
+            colaborador.Sexo = Convert.ToString(cbSexo.Text);
+            colaborador.Cargo = Convert.ToString(cbCargo.Text);
+            colaborador.Programador = Convert.ToBoolean(ckbProgramador.Text);
+
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\Banco-de-dados.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"UPDATE colaboratores SET nome = @NOME,cpf = @CPF, salario = @SALARIO, sexo = @SEXO, cargo = @CARGO,programador = @PROGRAMADOR WHERE id =@ID";
+            comando.Parameters.AddWithValue("@ID", colaborador.Id);
+            comando.Parameters.AddWithValue("@NOME", colaborador.Nome);
+            comando.Parameters.AddWithValue("@CPF", colaborador.Cpf);
+            comando.Parameters.AddWithValue("@SALARIO", colaborador.Salario);
+            comando.Parameters.AddWithValue("@SEXO", colaborador.Sexo);
+            comando.Parameters.AddWithValue("@CARGO", colaborador.Cargo);
+            comando.Parameters.AddWithValue("@PROGRAMADOR", colaborador.Programador);
+            comando.ExecuteNonQuery();
+            conexao.Close();
+            AtualizarTabela();
+            LimparCampos();
+
+        }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -100,6 +169,45 @@ namespace Exercicio01
         private void mtbCPF_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
 
+        }
+
+        private void dgvColaboradores_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = Convert.ToInt32(dgvColaboradores.CurrentRow.Cells[0].Value);
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\Banco-de-dados.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.Open();
+            SqlCommand comando = new SqlCommand();
+            comando.CommandText = @"SELECT id,nome,cpf,salario,sexo,cargo,programador FROM colaboradores WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            comando.Connection = conexao;
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+            DataRow linha = tabela.Rows[0];
+            Colaborador colaborador = new Colaborador();
+            colaborador.Id = Convert.ToInt32(linha["id"]);
+            colaborador.Nome = linha["nome"].ToString();
+            colaborador.Cpf = linha["cpf"].ToString();
+            colaborador.Salario = Convert.ToDecimal(linha["salario"]);
+            colaborador.Sexo = linha["sexo"].ToString();
+            colaborador.Cargo = linha["cargo"].ToString();
+            colaborador.Programador = Convert.ToBoolean(linha["programador"]);
+
+            lblId.Text = colaborador.Id.ToString();
+            txtNome.Text = colaborador.Nome.ToString();
+            mtbCPF.Text = colaborador.Cpf.ToString();
+            mtbSalario.Text = colaborador.Salario.ToString();
+            cbSexo.Text = colaborador.Sexo.ToString();
+            cbCargo.Text = colaborador.Sexo.ToString();
+            ckbProgramador.Text = colaborador.Programador.ToString();
+            conexao.Close();
+
+
+        }
+
+        private void ColaboradoresForm_Load(object sender, EventArgs e)
+        {
+            AtualizarTabela();
         }
     }
 }
