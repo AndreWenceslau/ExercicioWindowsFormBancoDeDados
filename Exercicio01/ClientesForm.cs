f587c316f3165f98f5dd1291a4155b548a8cc775
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-06-21 19:03:20 -0300

    Alterar finalizado

--- Exercicio01/ClientesForm.cs
+++ Exercicio01/ClientesForm.cs
@@ -25,13 +25,13 @@ namespace Exercicio01
 
         private void btnApagar_Click(object sender, EventArgs e)
         {
-            if(lblId.Text == "0")
+            if (lblId.Text == "0")
             {
                 Inserir();
             }
             else
             {
-                //Alterar();
+                Alterar();
             }
         }
         string texto = "";
@@ -126,7 +126,7 @@ namespace Exercicio01
                 cliente.Logradouro = linha["logradouro"].ToString();
                 cliente.Numero = Convert.ToInt32(linha["numero"]);
                 cliente.Complexo = linha["complexo"].ToString();
-                if(cliente.Nome_sujo ==true)
+                if (cliente.Nome_sujo == true)
                 {
                     texto = "sim";
                 }
@@ -136,13 +136,61 @@ namespace Exercicio01
                 }
                 cliente.Altura = Convert.ToDecimal(linha["altura"]);
                 cliente.Peso = Convert.ToDecimal(linha["peso"]);
+
                 dgvClientes.Rows.Add(new string[] { cliente.Id.ToString(), cliente.Nome.ToString(), cliente.Cpf.ToString(), cliente.Salario.ToString(), cliente.Telefone.ToString(), cliente.Estado.ToString(), cliente.Cidade.ToString(), cliente.Bairro.ToString(), cliente.Cep.ToString(), cliente.Logradouro.ToString(), cliente.Numero.ToString(), cliente.Complexo.ToString(), texto, cliente.Altura.ToString(), cliente.Peso.ToString() });
             }
-            
+
         }
         private void Alterar()
         {
+            Cliente cliente = new Cliente();
+            cliente.Id = Convert.ToInt32(lblId.Text);
+            cliente.Nome = Convert.ToString(txtNome.Text);
+            cliente.Cpf = Convert.ToString(mtbCpf.Text);
+            cliente.Salario = Convert.ToDecimal(mtbSalario.Text.Replace("R$", ""));
+            cliente.Telefone = Convert.ToString(mtbTelefone.Text);
+            cliente.Estado = Convert.ToString(txtEstado.Text);
+            cliente.Cidade = Convert.ToString(txtCidade.Text);
+            cliente.Bairro = Convert.ToString(txtBairro.Text);
+            cliente.Cep = Convert.ToString(mtbCep.Text);
+            cliente.Logradouro = Convert.ToString(txtLogradouro.Text);
+            cliente.Numero = Convert.ToInt32(txtNumero.Text);
+            if(ckbNomeSujo.Checked)
+            {
+                cliente.Nome_sujo = true;
+            }
+            else
+            {
+                cliente.Nome_sujo = false;
+            }
+            cliente.Complexo = Convert.ToString(txtComplexo.Text);
+            cliente.Altura = Convert.ToDecimal(txtAltura.Text);
+            cliente.Peso = Convert.ToDecimal(txtPeso.Text);
 
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @""; // Colocar o Banco
+            conexao.Open();
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"UPDATE clientes SET nome= @NOME,cpf = @CPF, salario = @SALARIO, telefone = @TELEFONE, estado = @ESTADO, cidade = @CIDADE, bairro = @BAIRRO, cep = @CEP,logradouro = @LOGRADOURO, numero = @Numero, nome_sujo = @NOME_SUJO, complexo = @COMPLEXO, altura = @ALTURA, peso = @PESO WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", cliente.Id);
+            comando.Parameters.AddWithValue("@NOME", cliente.Nome);
+            comando.Parameters.AddWithValue("@CPF", cliente.Cpf);
+            comando.Parameters.AddWithValue("@SALARIO", cliente.Salario);
+            comando.Parameters.AddWithValue("@TELEFONE", cliente.Telefone);
+            comando.Parameters.AddWithValue("@ESTADO", cliente.Estado);
+            comando.Parameters.AddWithValue("@CIDADE", cliente.Cidade);
+            comando.Parameters.AddWithValue("@BAIRRO", cliente.Bairro);
+            comando.Parameters.AddWithValue("@CEP", cliente.Cep);
+            comando.Parameters.AddWithValue("@LOGRADOURO", cliente.Logradouro);
+            comando.Parameters.AddWithValue("@NUMERO", cliente.Numero);
+            comando.Parameters.AddWithValue("@NOME_SUJO", cliente.Nome_sujo);
+            comando.Parameters.AddWithValue("@COMPLEXO", cliente.Complexo);
+            comando.Parameters.AddWithValue("@ALTURA", cliente.Altura);
+            comando.Parameters.AddWithValue("@PESO", cliente.Peso);
+            comando.ExecuteNonQuery();
+            LimparCampos();
+            AtualizarTabela();
         }
     }
 }
